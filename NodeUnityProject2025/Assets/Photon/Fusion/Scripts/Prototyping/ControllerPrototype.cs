28bc397973b8adefb98411f4ba2c3e24c63f108c
LDelDeo <ljdeldeo@email.NEIT.edu>
2025-03-05 21:08:19 -0500

    SceneUpgrade

--- NodeUnityProject2025/Assets/Photon/Fusion/Scripts/Prototyping/ControllerPrototype.cs
+++ NodeUnityProject2025/Assets/Photon/Fusion/Scripts/Prototyping/ControllerPrototype.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using Fusion;
 
 [ScriptHelp(BackColor = EditorHeaderBackColor.Steel)]
-public class ControllerPrototype : Fusion.NetworkBehaviour {
+public class ControllerPrototype : NetworkBehaviour {
     protected NetworkCharacterControllerPrototype _ncc;
     protected NetworkRigidbody _nrb;
     protected NetworkRigidbody2D _nrb2d;
@@ -11,19 +11,14 @@ public class ControllerPrototype : Fusion.NetworkBehaviour {
     [Networked]
     public Vector3 MovementDirection { get; set; }
 
-    public bool TransformLocal = false;
-
-    [DrawIf(nameof(ShowSpeed), Hide = true)]
     public float Speed = 6f;
-    public float MouseSensitivity = 100f;
-
+    public float MouseSensitivity = 400f; // Increased sensitivity
     private float _rotationX = 0f;
-
-    bool ShowSpeed => this && !TryGetComponent<NetworkCharacterControllerPrototype>(out _);
+    private bool isCursorLocked = true;
 
     public void Awake() {
         CacheComponents();
-        Cursor.lockState = CursorLockMode.Locked;
+        LockCursor(true);
     }
 
     public override void Spawned() {
@@ -31,10 +26,10 @@ public class ControllerPrototype : Fusion.NetworkBehaviour {
     }
 
     private void CacheComponents() {
-        if (!_ncc) _ncc     = GetComponent<NetworkCharacterControllerPrototype>();
-        if (!_nrb) _nrb     = GetComponent<NetworkRigidbody>();
+        if (!_ncc) _ncc = GetComponent<NetworkCharacterControllerPrototype>();
+        if (!_nrb) _nrb = GetComponent<NetworkRigidbody>();
         if (!_nrb2d) _nrb2d = GetComponent<NetworkRigidbody2D>();
-        if (!_nt) _nt       = GetComponent<NetworkTransform>();
+        if (!_nt) _nt = GetComponent<NetworkTransform>();
     }
 
     public override void FixedUpdateNetwork() {
@@ -42,58 +37,78 @@ public class ControllerPrototype : Fusion.NetworkBehaviour {
             return;
         }
 
-        Vector3 direction = Vector3.zero;
+        if (Object.HasInputAuthority) { 
+            HandleMouseLook();
+            HandleCursorLockToggle();
+        }
+
+        Vector3 moveDirection = Vector3.zero;
+
         if (GetInput(out NetworkInputPrototype input)) {
             if (input.IsDown(NetworkInputPrototype.BUTTON_FORWARD)) {
-                direction += transform.forward;
+                moveDirection += transform.forward;  // Move forward relative to player rotation
             }
             if (input.IsDown(NetworkInputPrototype.BUTTON_BACKWARD)) {
-                direction -= transform.forward;
+                moveDirection -= transform.forward;  // Move backward relative to player rotation
             }
             if (input.IsDown(NetworkInputPrototype.BUTTON_LEFT)) {
-                direction -= transform.right;
+                moveDirection -= transform.right;  // Move left relative to player rotation
             }
             if (input.IsDown(NetworkInputPrototype.BUTTON_RIGHT)) {
-                direction += transform.right;
+                moveDirection += transform.right;  // Move right relative to player rotation
             }
 
-            direction = direction.normalized;
-            MovementDirection = direction;
+            moveDirection = moveDirection.normalized;
+            MovementDirection = moveDirection;
 
             if (input.IsDown(NetworkInputPrototype.BUTTON_JUMP)) {
                 if (_ncc) {
                     _ncc.Jump();
                 } else {
-                    direction += Vector3.up;
+                    moveDirection += Vector3.up;
                 }
             }
         } else {
-            direction = MovementDirection;
+            moveDirection = MovementDirection;
         }
 
         if (_ncc) {
-            _ncc.Move(direction * Speed);
+            _ncc.Move(moveDirection * Speed);
         } else if (_nrb && !_nrb.Rigidbody.isKinematic) {
-            _nrb.Rigidbody.AddForce(direction * Speed);
+            _nrb.Rigidbody.AddForce(moveDirection * Speed);
         } else if (_nrb2d && !_nrb2d.Rigidbody.isKinematic) {
-            Vector2 direction2d = new Vector2(direction.x, direction.z);
+            Vector2 direction2d = new Vector2(moveDirection.x, moveDirection.z);
             _nrb2d.Rigidbody.AddForce(direction2d * Speed);
         } else {
-            transform.position += direction * Speed * Runner.DeltaTime;
+            transform.position += moveDirection * Speed * Runner.DeltaTime;
         }
     }
 
-    public  void Update() {
-        HandleMouseLook();
-    }
-
     private void HandleMouseLook() {
+        if (!Object.HasInputAuthority || !isCursorLocked) return;
+
         float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
 
         _rotationX -= mouseY;
         _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(_rotationX, transform.localRotation.eulerAngles.y + mouseX, 0f);
+        // Rotate only the camera up/down
+        Camera.main.transform.localRotation = Quaternion.Euler(_rotationX, 0f, 0f);
+        
+        // Rotate the player left/right
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void HandleCursorLockToggle() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isCursorLocked = !isCursorLocked;
+            LockCursor(isCursorLocked);
+        }
+    }
+
+    private void LockCursor(bool shouldLock) {
+        Cursor.lockState = shouldLock ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !shouldLock;
     }
 }
